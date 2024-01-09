4a1bf8d7f9c3af3bb3e1ca1221e182d7f189c2ef
Ugo RODRIGUEZ <ugo.rodriguez@mobile2-28.cpe.lan>
2024-01-09 15:27:35 +0100

    Add Sphere class

---
+++ PathTracing/src/Renderer/PathTracing/Primitives/Sphere.h
@@ -0,0 +1,35 @@
+#ifndef Sphere_HPP
+#define Sphere_HPP
+
+#include "Primitive.h"
+
+namespace PathTracing
+{
+
+class Sphere : public Primitive
+{
+public:
+
+    Sphere(glm::vec3 const& center_param,float radius_param);
+
+    /** Center of the Sphere */
+    glm::vec3 const& center() const;
+    /** Radius of the Sphere */
+    float radius() const;
+
+    /** Intersection computation with a ray */
+    //bool intersect(Ray const& ray_param,intersection_data& intersection) const override;
+
+
+private:
+
+    /** Center of the Sphere */
+    glm::vec3 center_data;
+    /** Radius of the Sphere */
+    float radius_data;
+};
+
+
+}
+
+#endif
