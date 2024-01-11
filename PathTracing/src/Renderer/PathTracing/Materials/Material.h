68fec1a6db61a02cb5cace2a63f86530499379c3
Ugo RODRIGUEZ <ugo.rodriguez@mobile2-28.cpe.lan>
2024-01-11 10:36:14 +0100

    Add light material

--- PathTracing/src/Renderer/PathTracing/Materials/Material.h
+++ PathTracing/src/Renderer/PathTracing/Materials/Material.h
@@ -14,6 +14,9 @@ public:
     virtual float brdf(glm::vec3 x,float theta_out,float theta_in,float lambda) const = 0;
     glm::vec3 get_albedo(glm::vec3 x,float theta_out,float theta_in,float lambda) {return color;};
 
+    virtual bool CanEmit() {return false;};
+    virtual glm::vec3 emitted() const {return glm::vec3(0.0f,0.0f,0.0f);};
+
 private:
     glm::vec3 color;
 };
