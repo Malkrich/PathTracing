8a8c305a3c82fdd5f8c0ca71417815db916a8e33
Malkrich <mathurin.lemarie@gmail.com>
2024-01-11 15:22:42 +0100

    making image class abstract to create 2 types of images

--- PathTracing/src/Renderer/Image.h
+++ PathTracing/src/Renderer/Image.h
@@ -1,37 +1,81 @@
 #pragma once
 
+#include <cstdint>
 #include <vector>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 namespace PathTracing
 {
 
+enum class ImageType
+{
+    Image32F, Image32UI
+};
+
 class Image
 {
 public:
-    Image();
     Image(unsigned int width, unsigned int height);
-
-    const void* getData() const { return (void*)glm::value_ptr(m_data[0]); }
+    virtual ~Image() {}
 
     unsigned int getWidth() const { return m_width; }
     unsigned int getHeight() const { return m_height; }
 
-    void resize(unsigned int width, unsigned int height);
+    void setData(unsigned int x, unsigned int y, float r, float g, float b);
+    void setData(unsigned int x, unsigned int y, const glm::vec3& color);
+    void setData(unsigned int x, unsigned int y, const glm::vec4& color);
 
-    glm::vec3& operator()(unsigned int x, unsigned int y);
-    const glm::vec3& operator()(unsigned int x, unsigned int y) const;
+    // Interface
+    virtual inline ImageType getType() const = 0;
+    virtual void resize(unsigned int width, unsigned int height) = 0;
+    virtual const void* getData() const = 0;
+    virtual void setData(unsigned int x, unsigned int y, float r, float g, float b, float a) = 0;
 
-    glm::vec3& operator[](unsigned int i);
-    const glm::vec3& operator[](unsigned int i) const;
+protected:
+    unsigned int m_width, m_height;
+};
+
+class Image32F : public Image
+{
+public:
+    Image32F(unsigned int width, unsigned int height);
+
+    virtual inline ImageType getType() const override { return ImageType::Image32F; }
+    virtual void resize(unsigned int width, unsigned int height) override;
+    virtual const void* getData() const override
+    {
+        return (void*)glm::value_ptr(m_data[0]);
+    }
+    virtual void setData(unsigned int x, unsigned int y, float r, float g, float b, float a) override;
 
 private:
     void clearData();
 
 private:
-    unsigned int m_width, m_height;
-    std::vector<glm::vec3> m_data;
+    std::vector<glm::vec4> m_data;
+};
+
+class Image32UI : public Image
+{
+public:
+    Image32UI(unsigned int width, unsigned int height);
+    virtual ~Image32UI();
+
+    virtual inline ImageType getType() const override { return ImageType::Image32UI; }
+    virtual void resize(unsigned int width, unsigned int height) override;
+    virtual const void* getData() const override
+    {
+        return (void*)m_data;
+    }
+    virtual void setData(unsigned int x, unsigned int y, float r, float g, float b, float a) override;
+
+private:
+    void clearData();
+
+private:
+    uint32_t* m_data = nullptr;
 };
 
 }
